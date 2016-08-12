7114622d2ebe32920fe3a9f60f29f415c702c80e
hengchao0248 <hengchao0248@163.com>
2016-08-12 17:50:54 +0800

    add y baseline

---
+++ y_baseline.py
@@ -0,0 +1,223 @@
+import pandas as pd
+import numpy as np
+import re
+import xgboost as xgb
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.preprocessing import LabelEncoder
+
+##############
+#load dataset#
+##############
+df_tr = pd.read_csv('./train/train_labels.txt',sep=u'|',header=None).dropna(1)
+df_tr.columns = ['uid','sex','age','loc']
+df_te = pd.read_csv('./test/test_nolabels.txt',sep=u'|',header=None).dropna(1)
+df_te.columns = ['uid']
+df_all = pd.concat([df_tr,df_te],axis=0)
+
+df_tr_info = pd.read_csv('./train/train_info.txt',sep=u'|',header=None).dropna(1)
+df_tr_info.columns = ['uid','name','image']
+df_tr_info = df_tr_info.drop_duplicates()
+df_te_info = pd.read_csv('./test/test_info.txt',sep=u'|',header=None).dropna(1)
+df_te_info.columns = ['uid','name','image']
+df_te_info = df_te_info.drop_duplicates()
+df_info = pd.concat([df_tr_info,df_te_info],axis=0)
+
+links = []
+for i, line in enumerate(open('./train/train_links.txt')):
+    line = line.split()
+    row = {'uid':int(line[0]),'fans_cnt':len(line)-1,'fans':' '.join(line[1:])}
+    links.append(row)
+df_tr_links = pd.DataFrame(links)
+df_tr_links = df_tr_links.drop_duplicates()
+
+links = []
+for i, line in enumerate(open('./test/test_links.txt')):
+    line = line.split()
+    row = {'uid':int(line[0]),'fans_cnt':len(line)-1,'fans':' '.join(line[1:])}
+    links.append(row)
+df_te_links = pd.DataFrame(links)
+df_te_links = df_te_links.drop_duplicates()
+
+df_links = pd.concat([df_tr_links,df_te_links],axis=0)
+
+status = []
+for i, line in enumerate(open('./train/train_status.txt')):
+    
+    l = re.search(',',line).span()[0]
+    r = re.search(',',line).span()[1]
+    row = {'uid':int(line[:l]),'sta':line[r:]}
+    status.append(row)
+df_tr_status = pd.DataFrame(status)
+
+status = []
+for i, line in enumerate(open('./test/test_status.txt')):
+    
+    l = re.search(',',line).span()[0]
+    r = re.search(',',line).span()[1]
+    row = {'uid':int(line[:l]),'sta':line[r:]}
+    status.append(row)
+df_te_status = pd.DataFrame(status)
+
+df_status = pd.concat([df_tr_status,df_te_status],axis=0)
+
+df_mge = pd.merge(df_all,df_info,on='uid',how='left')
+df_mge = pd.merge(df_mge,df_links,on='uid',how='left')
+df_mge.index = range(len(df_mge))
+
+##################
+#processing label#
+##################
+d = {'上海': '华东',
+ '云南': '西南',
+ '内蒙古': '华北',
+ '北京': '华北',
+ '台湾': '华东',
+ '吉林': '东北',
+ '四川': '西南',
+ '天津': '华北',
+ '宁夏': '西北',
+ '安徽': '华东',
+ '山东': '华东',
+ '山西': '华北',
+ '广东': '华南',
+ '广西': '华南',
+ '新疆': '西北',
+ '江苏': '华东',
+ '江西': '华东',
+ '河北': '华北',
+ '河南': '华中',
+ '浙江': '华东',
+ '海南': '华南',
+ '湖北': '华中',
+ '湖南': '华中',
+ '澳门': '华南',
+ '甘肃': '西北',
+ '福建': '华东',
+ '西藏': '西南',
+ '贵州': '西南',
+ '辽宁': '东北',
+ '重庆': '西南',
+ '陕西': '西北',
+ '青海': '西北',
+ '香港': '华南',
+ '黑龙江': '东北'}
+
+def bin_loc(s):
+    if pd.isnull(s):
+        return s
+    s = s.split(' ')[0]
+    if s == 'None':
+        return '华北'
+    if s == '海外':
+        return s
+    return d[s]
+
+def bin_age(age):
+    if pd.isnull(age):
+        return age
+    if age <=1979:
+        return "-1979"
+    elif age<=1989:
+        return "1980-1989"
+    else:
+        return "1990+"
+    
+def mlogloss(yhat,y):
+    return np.mean([-np.log(yhat[i,_y]) for i,_y in enumerate(y)])
+def macc(yhat,y):
+    return np.mean(yhat.argmax(axis=1) == y)
+
+df_mge['loc_bin'] = df_mge['loc'].map(bin_loc)
+df_mge['age_bin'] = df_mge['age'].map(bin_age)
+
+age_le = LabelEncoder()
+y_age = age_le.fit_transform(df_mge.iloc[:3200]['age_bin'])
+
+loc_le = LabelEncoder()
+y_loc = loc_le.fit_transform(df_mge.iloc[:3200]['loc_bin'])
+
+sex_le = LabelEncoder()
+y_sex = sex_le.fit_transform(df_mge.iloc[:3200]['sex'])
+
+######################
+# y baseline #########
+######################
+
+prds = []
+
+y = y_age
+d = pd.Series(y).value_counts().to_dict()
+yhat = np.zeros((3200,len(d)))
+for k in d:
+    yhat[:,k] = d[k]/3200
+print('mlogloss:',mlogloss(yhat,y))
+print('macc:',macc(yhat,y))
+prds.append(yhat)
+prds.append(y)
+
+y = y_sex
+d = pd.Series(y).value_counts().to_dict()
+yhat = np.zeros((3200,len(d)))
+for k in d:
+    yhat[:,k] = d[k]/3200
+print('mlogloss:',mlogloss(yhat,y))
+print('macc:',macc(yhat,y))
+prds.append(yhat)
+prds.append(y)
+
+y = y_loc
+d = pd.Series(y).value_counts().to_dict()
+yhat = np.zeros((3200,len(d)))
+for k in d:
+    yhat[:,k] = d[k]/3200
+print('mlogloss:',mlogloss(yhat,y))
+print('macc:',macc(yhat,y))
+prds.append(yhat)
+prds.append(y)
+
+def lb_score(yhat_age,y_age,yhat_sex,y_sex,yhat_loc,y_loc):
+    print('age mlogloss:',mlogloss(yhat_age,y_age))
+    a1 = macc(yhat_age,y_age)
+    print('age macc:',a1)
+    
+    print('sex mlogloss:',mlogloss(yhat_sex,y_sex))
+    a2 = macc(yhat_sex,y_sex)
+    print('sex macc:',a2)
+    
+    print('loc mlogloss:',mlogloss(yhat_loc,y_loc))
+    a3 = macc(yhat_loc,y_loc)
+    print('loc macc:',a3)
+    
+    print('LB score:',0.3*a1+0.2*a2+0.5*a3)
+
+lb_score(prds[0],prds[1],prds[2],prds[3],prds[4],prds[5])
+
+############
+#submission
+############
+df_sub = pd.DataFrame()
+df_sub['uid'] = df_mge.iloc[3200:]['uid']
+n = len(df_sub)
+df_sub['age'] = age_le.inverse_transform(prds[0][:n].argmax(axis=1))
+df_sub['gender'] = sex_le.inverse_transform(prds[2][:n].argmax(axis=1))
+df_sub['province'] = loc_le.inverse_transform(prds[4][:n].argmax(axis=1))
+df_sub.to_csv('./predictions/temp.csv',index=None)
+
+# mlogloss: 0.956884833064
+# macc: 0.5609375
+# mlogloss: 0.555364063882
+# macc: 0.75625
+# mlogloss: 1.73154423162
+# macc: 0.29875
+# age mlogloss: 0.956884833064
+# age macc: 0.5609375
+# sex mlogloss: 0.555364063882
+# sex macc: 0.75625
+# loc mlogloss: 1.73154423162
+# loc macc: 0.29875
+# LB score: 0.46890625
+
+# LB score:0.459489795918367.
